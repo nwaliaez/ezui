c82cdf57a90e14e26c6968fdd56bbc14a9ccf0be
Neeraj walia <ezsnippet@Neerajs-MacBook-Pro.local>
2024-11-10 05:52:12 +0530

    first commit

---
+++ src/app/(main)/docs/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+
+const DocsLayout = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold mb-4">Accordion</h1>
+      <p className="text-gray-400 mb-8">
+        Accordion display a list of high-level options that can
+        expand/collapseasd to reveal more information.
+      </p>
+    </>
+  );
+};
+
+export default DocsLayout;
